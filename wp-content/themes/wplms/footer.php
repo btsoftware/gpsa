a07d9ced5fa22f3361be8d152172563c5757ef76
CeciliaMadrid <lya.madrid@gmail.com>
2014-03-05 11:26:37 -0600

    Cambios en footer
    Estructura

--- wp-content/themes/wplms/footer.php
+++ wp-content/themes/wplms/footer.php
@@ -1,14 +1,22 @@
 <footer>
+    <div class="footer-contact">
+	<div class="container">
+	<div class="row">
+	<?php 
+		    if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar('topfootersidebar') ) : ?>
+	<?php endif; ?>
+	</div>
+	</div>
+    </div>
+
+    <div class="footertop">
     <div class="container">
         <div class="row">
-            <div class="footertop">
-                <?php 
-                            if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar('topfootersidebar') ) : ?>
-                <?php endif; ?>
-            </div>
-        </div>
-        <div class="row">
             <div class="footerbottom">
+		            <div class="col-md-5">
+                <h2 id="footerlogo"><a><img src="<?php $logo=vibe_get_option('logo'); echo (isset($logo)?$logo:VIBE_URL.'/images/logo.png'); ?>"></a></h2>
+                
+            </div>
                 <?php 
                     if ( !function_exists('dynamic_sidebar')|| !dynamic_sidebar('bottomfootersidebar') ) : ?>
                 <?php endif; ?>
@@ -22,21 +30,12 @@
 <div id="footerbottom">
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <h2 id="footerlogo"><a><img src="<?php $logo=vibe_get_option('logo'); echo (isset($logo)?$logo:VIBE_URL.'/images/logo.png'); ?>"></a></h2>
+            <div class="col-md-4">              
                 <?php $copyright=vibe_get_option('copyright'); echo (isset($copyright)?$copyright:'&copy; 2013, All rights reserved.'); ?>
             </div>
-            <div class="col-md-9">
+            <div class="col-md-8">
                 <div id="footermenu">
-                    <?php
-                            $args = array(
-                                'theme_location'  => 'footer-menu',
-                                'container'       => '',
-                                'menu_class'      => 'footermenu',
-                                'fallback_cb'     => 'vibe_set_menu',
-                            );
-                            wp_nav_menu( $args );
-                    ?>
+
                 </div>    
             </div>
         </div>
