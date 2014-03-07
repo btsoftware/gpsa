e30cb6b5a3ca9fcc14e20ffdbe0e849b8b626853
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-06 19:57:31 -0600

    debug wp-login.php

--- wp-login.php
+++ wp-login.php
@@ -781,19 +781,17 @@ default:
 		}
 
 		if ( ( empty( $redirect_to ) || $redirect_to == 'wp-admin/' || $redirect_to == admin_url() ) ) {
-			die("as");
 			// If the user doesn't belong to a blog, send them to user admin. If the user can't edit posts, send them to their profile.
-			if ( is_multisite() && !get_active_blog_for_user($user->ID) && !is_super_admin( $user->ID ) )
+			if ( is_multisite() && !get_active_blog_for_user($user->ID) && !is_super_admin( $user->ID ) ) die("1");
 				$redirect_to = user_admin_url();
-			elseif ( is_multisite() && !$user->has_cap('read') )
+			elseif ( is_multisite() && !$user->has_cap('read') ) die("2");
 				$redirect_to = get_dashboard_url( $user->ID );
-			elseif ( !$user->has_cap('edit_posts') )
+			elseif ( !$user->has_cap('edit_posts') ) die("3");
 				$redirect_to = admin_url('profile.php');
 			else
 				$redirect_to = home_url();
 		}
 		
-		die("noo");
 		wp_safe_redirect($redirect_to);
 		exit();
 	}
