5546428ecd37457a88c70c03dfdf3edd2f98f22b
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-12 17:02:06 -0600

    debug bookmars

--- wp-content/themes/wplms/page-add-bookmark.php
+++ wp-content/themes/wplms/page-add-bookmark.php
@@ -1,6 +1,12 @@
 <?php
 
 if(function_exists('bp_loggedin_user_link') && is_user_logged_in()) {
+	die(var_dump($_GET));
+	
+	if(isset($_GET["page"]) and isset($_GET["title"])) {
+		die(var_dump($_GET));
+	}
+	
 	global $wp;
 	
 	$current_url = add_query_arg($wp->query_string, '', home_url( $wp->request ));
