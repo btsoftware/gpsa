15a2c3c73d9167bef2af066140429674cd92ff71
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-12 17:58:50 -0600

    bookmarks wp insert - user_id

--- wp-content/themes/wplms/page-add-bookmark.php
+++ wp-content/themes/wplms/page-add-bookmark.php
@@ -9,7 +9,7 @@ if(function_exists('bp_loggedin_user_link') && is_user_logged_in()) {
 		$title       = $_GET["title"];
 		$id_user     = bp_loggedin_user_id();
 		
-		$wpdb->insert('wp_bookmarks', array('id_user' => $id_user, 'url' => $current_url, 'title' => $title));
+		$wpdb->insert('wp_bookmarks', array('user_id' => $id_user, 'url' => $current_url, 'title' => $title));
 		
 		header('Location: '. $current_url);
 		exit();
