77f79d83ddd01e31f49a9d9e862443a44ea01e9b
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-12 16:51:13 -0600

    insert page custom ad bookmark

---
+++ wp-content/themes/wplms/page-add-bookmark.php
@@ -0,0 +1,13 @@
+<?php
+
+if(function_exists('bp_loggedin_user_link') && is_user_logged_in()) {
+	global $wp;
+	
+	$current_url = add_query_arg($wp->query_string, '', home_url( $wp->request ));
+	$current_url = explode("add-bookmark/", $current_url);
+	$current_url = $current_url[1];
+	$id_user     = bp_member_user_id();
+	
+	die(var_dump($current_url));
+	$wpdb->insert('bookmarks', array('id_user' => $id_user, 'value' => $current_url));
+}
