db73386848784e27be55ba43260fc2c780468b69
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-12 17:10:14 -0600

    debug bookmars

--- wp-content/themes/wplms/page-add-bookmark.php
+++ wp-content/themes/wplms/page-add-bookmark.php
@@ -2,16 +2,18 @@
 
 if(function_exists('bp_loggedin_user_link') && is_user_logged_in()) {	
 	if(isset($_GET["page"]) and isset($_GET["title"])) {
-		die(var_dump($_GET));
-		
 		global $wp;
-		$current_url = add_query_arg($wp->query_string, '', home_url( $wp->request ));
-		$current_url = explode("add-bookmark/", $current_url);
-		$current_url = $current_url[1];
+		global $wpdb;
+		
+		$current_url = $_GET["page"];
+		$title       = $_GET["title"];
 		$id_user     = bp_member_user_id();
 		
-		die(var_dump($current_url));
-		$wpdb->insert('bookmarks', array('id_user' => $id_user, 'value' => $current_url));
+		die(var_dump($current_url . $title . $id_user));
+		
+		header('Location: '. $current_url);
+		exit();
+		//$wpdb->insert('bookmarks', array('id_user' => $id_user, 'value' => $current_url));
 	} else {
 		header('Location: '. home_url());
 		exit();
