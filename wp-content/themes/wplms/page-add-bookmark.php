046efc63e96cf1ab07cbd4103599a393a80158e8
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-12 18:18:30 -0600

    return msg

--- wp-content/themes/wplms/page-add-bookmark.php
+++ wp-content/themes/wplms/page-add-bookmark.php
@@ -11,7 +11,7 @@ if(function_exists('bp_loggedin_user_link') && is_user_logged_in()) {
 		
 		$wpdb->insert('wp_bookmarks', array('user_id' => $id_user, 'url' => $current_url, 'title' => $title));
 		
-		header('Location: '. $current_url);
+		header('Location: '. $current_url . "?msg=successful-bookmark");
 		exit();
 	} else {
 		header('Location: '. home_url());
