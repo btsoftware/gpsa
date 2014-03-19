d185b65e3de2129afa95bc539df1700f243cea29
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-19 16:59:06 -0600

    geojson.js

---
+++ map/gpsa-rosters.geojson.js
@@ -0,0 +1,198 @@
+var geojson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Hernan Charosky",
+          "Karina Banfi"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1",
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,3,4,5,6,8",
+          "1,3,6,8"
+        ],
+        "name": "Argentina"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -58.447265625,
+          -34.63772760271711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Almudena Ocejo"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "4,5,8"
+        ],
+        "name": "Mexico"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -99.13324356079102,
+          19.43268142267861
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Aly Elias Lalá"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,2,3,5,6"
+        ],
+        "name": "Mozambique"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          34.5849609375,
+          -18.0936442705026
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Anabel Cruz"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,3,5,6,8"
+        ],
+        "name": "Uruguay"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -55.37109374999999,
+          -32.39851580247402
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Bibhu Prasad Sahu"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,2,4,5,8"
+        ],
+        "name": "India"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          79.38720703125,
+          22.715390019335942
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Kedar Khadka"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,2,3,4,5,6,8"
+        ],
+        "name": "Nepal"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          85.35827636718749,
+          27.70784710660343
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "names": [
+          "Richard Holloway"
+        ],
+        "urls": [
+          "http://gpsa.fundarlabs.mx/v1"
+        ],
+        "themes": [
+          "1,2,3,4,5,6,7,8"
+        ],
+        "name": "France"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          2.35107421875,
+          48.85387273165656
+        ]
+      }
+    }
+  ],
+  "properties": {
+    "themes": [
+      {
+        "id_theme": 1,
+        "name": "Access to information"
+      },
+      {
+        "id_theme": 2,
+        "name": "Budget"
+      },
+      {
+        "id_theme": 3,
+        "name": "State oversight mechanisms"
+      },
+      {
+        "id_theme": 4,
+        "name": "Citizen oversight mechanisms"
+      },
+      {
+        "id_theme": 5,
+        "name": "Citizen/Beneficiary feedback mechanisms"
+      },
+      {
+        "id_theme": 6,
+        "name": "Multi-stakeholder engagement"
+      },
+      {
+        "id_theme": 7,
+        "name": "Political economy"
+      },
+      {
+        "id_theme": 8,
+        "name": "Advocacy and activism"
+      }
+    ]
+  }
+};
