6769ca76fb02ef539d9ef58b422bb6099570b151
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-26 13:08:03 -0600

    custom page

---
+++ wp-content/themes/wplms/page-join-gpsa.php
@@ -0,0 +1,31 @@
+<?php
+get_header();
+?>
+<section id="title">
+	<div class="container">
+		<div class="row">
+            <div class="col-md-12">
+                <div class="pagetitle">
+                    <h1><?php echo get_bloginfo('name'); ?></h1>
+                    <h5><?php  echo get_bloginfo('description'); ?></h5>
+                </div>
+            </div>
+        </div>
+	</div>
+</section>
+
+<section id="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="no content">
+					<?php getMap();?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+get_footer();
+?>
