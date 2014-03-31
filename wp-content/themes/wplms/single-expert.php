a959e2812d8895720233b72b19ee9b077163aa7e
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-03-31 16:38:46 -0600

    div

--- wp-content/themes/wplms/single-expert.php
+++ wp-content/themes/wplms/single-expert.php
@@ -32,70 +32,71 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
         <div class="row">
             <div class="col-md-9 col-sm-8">
                 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <div class="content">
-                    <div class="bp-widget wp-profile">
-						
-						<h4><?php echo $data["name"];?></h4>
+					<div class="content">
+						<div class="bp-widget wp-profile">
+							
+							<h4><?php echo $data["name"];?></h4>
 
-						<table class="wp-profile-fields">
+							<table class="wp-profile-fields">
 
-							<?php if ( $ud->display_name ) : ?>
+								<?php if ( $ud->display_name ) : ?>
 
-								<tr id="wp_displayname">
-									<td class="label"><?php _e( 'Name', 'vibe' ); ?></td>
-									<td class="data"><?php echo $ud->display_name; ?></td>
-								</tr>
+									<tr id="wp_displayname">
+										<td class="label"><?php _e( 'Name', 'vibe' ); ?></td>
+										<td class="data"><?php echo $ud->display_name; ?></td>
+									</tr>
 
-							<?php endif; ?>
+								<?php endif; ?>
 
-							<?php if ( $ud->user_description ) : ?>
+								<?php if ( $ud->user_description ) : ?>
 
-								<tr id="wp_desc">
-									<td class="label"><?php _e( 'About Me', 'vibe' ); ?></td>
-									<td class="data"><?php echo $ud->user_description; ?></td>
-								</tr>
+									<tr id="wp_desc">
+										<td class="label"><?php _e( 'About Me', 'vibe' ); ?></td>
+										<td class="data"><?php echo $ud->user_description; ?></td>
+									</tr>
 
-							<?php endif; ?>
+								<?php endif; ?>
 
-							<?php if ( $ud->user_url ) : ?>
+								<?php if ( $ud->user_url ) : ?>
 
-								<tr id="wp_website">
-									<td class="label"><?php _e( 'Website', 'vibe' ); ?></td>
-									<td class="data"><?php echo make_clickable( $ud->user_url ); ?></td>
-								</tr>
+									<tr id="wp_website">
+										<td class="label"><?php _e( 'Website', 'vibe' ); ?></td>
+										<td class="data"><?php echo make_clickable( $ud->user_url ); ?></td>
+									</tr>
 
-							<?php endif; ?>
+								<?php endif; ?>
 
-							<?php if ( $ud->jabber ) : ?>
+								<?php if ( $ud->jabber ) : ?>
 
-								<tr id="wp_jabber">
-									<td class="label"><?php _e( 'Jabber', 'vibe' ); ?></td>
-									<td class="data"><?php echo $ud->jabber; ?></td>
-								</tr>
+									<tr id="wp_jabber">
+										<td class="label"><?php _e( 'Jabber', 'vibe' ); ?></td>
+										<td class="data"><?php echo $ud->jabber; ?></td>
+									</tr>
 
-							<?php endif; ?>
+								<?php endif; ?>
 
-							<?php if ( $ud->aim ) : ?>
+								<?php if ( $ud->aim ) : ?>
 
-								<tr id="wp_aim">
-									<td class="label"><?php _e( 'AOL Messenger', 'vibe' ); ?></td>
-									<td class="data"><?php echo $ud->aim; ?></td>
-								</tr>
+									<tr id="wp_aim">
+										<td class="label"><?php _e( 'AOL Messenger', 'vibe' ); ?></td>
+										<td class="data"><?php echo $ud->aim; ?></td>
+									</tr>
 
-							<?php endif; ?>
+								<?php endif; ?>
 
-							<?php if ( $ud->yim ) : ?>
+								<?php if ( $ud->yim ) : ?>
 
-								<tr id="wp_yim">
-									<td class="label"><?php _e( 'Yahoo Messenger', 'vibe' ); ?></td>
-									<td class="data"><?php echo $ud->yim; ?></td>
-								</tr>
+									<tr id="wp_yim">
+										<td class="label"><?php _e( 'Yahoo Messenger', 'vibe' ); ?></td>
+										<td class="data"><?php echo $ud->yim; ?></td>
+									</tr>
 
-							<?php endif; ?>
-						<?php endwhile;?>
-						</table>
+								<?php endif; ?>
+							<?php endwhile;?>
+							</table>
+						</div>
 					</div>
-                </div>
+				</div>
             </div>
             
             <div class="col-md-3 col-sm-3">
