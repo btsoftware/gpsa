3984083833e2e22ec912994479f67fc83effc517
CeciliaMadrid <lya.madrid@gmail.com>
2014-04-02 09:58:02 -0600

    publicaciones

--- wp-content/themes/wplms/single-nosidebar.php
+++ wp-content/themes/wplms/single-nosidebar.php
@@ -15,13 +15,13 @@ if(isset($title) && $title !='' && $title !='H'){
 <section id="title">
     <div class="container">
         <div class="row">
-            <div class="col-md-9 col-sm-8">
+            <div class="col-md-3 col-sm-4">           
                 <div class="pagetitle">
                     <h1><?php the_title(); ?></h1>
                     <h5><?php the_sub_title(); ?></h5>
                 </div>
             </div>
-             <div class="col-md-3 col-sm-4">
+            <div class="col-md-9 col-sm-8">
                  <?php
                     $breadcrumbs=get_post_meta(get_the_ID(),'vibe_breadcrumbs',true);
                     if(isset($breadcrumbs) && $breadcrumbs !='' && $breadcrumbs !='H'){
