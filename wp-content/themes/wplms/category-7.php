a5c104f455e4dc5d1006ee0aed67e02a75e500d4
CeciliaMadrid <lya.madrid@gmail.com>
2014-04-02 17:11:40 -0600

    Carrusel repositorio
    cambio de

--- wp-content/themes/wplms/category-7.php
+++ wp-content/themes/wplms/category-7.php
@@ -20,7 +20,7 @@ get_header();
 <section class="stripe top">
 	<div class="container">
 		<div class="row">	
-	               <?php echo do_shortcode('[wonderplugin_carousel id="1"]'); ?>
+	               <?php echo get_touchcarousel(2); ?>
 		</div>
 	</div>
 </section>
