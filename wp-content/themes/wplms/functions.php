dfd5a815aad820fcc4c1c67343d5f5ce3aa2896f
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-04-01 20:11:24 -0600

    menu - ancla roster

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -101,8 +101,8 @@ function getMap() {
 	echo "<link href='/map/css/map-style.css' rel='stylesheet'/>";
 	echo "<script src='/map/js/gpsa-rosters.geojson.js' type='text/javascript'></script>";
 	
-	echo "<div class='pagetitle'><h2>Roster of practitioners</h2></div>";
-	echo "<a name='roster-of-practitioners'></a><div id='map'><div id='themes-layers' class='layers'></div><div id='info'></div></div>";
+	echo "<a name='roster-of-practitioners'></a><div class='pagetitle'><h2>Roster of practitioners</h2></div>";
+	echo "<div id='map'><div id='themes-layers' class='layers'></div><div id='info'></div></div>";
 	echo "<script src='/map/js/map-init.js' type='text/javascript'></script>";
 }
 
