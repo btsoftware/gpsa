3272e3a2d6b7d823c4cd893d670dc14d5786c020
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-04-02 15:59:18 -0600

    footer forum link ref

--- wp-content/themes/wplms/footer.php
+++ wp-content/themes/wplms/footer.php
@@ -125,6 +125,7 @@ echo vibe_get_option('google_analytics');
 ?>
 <script type="text-javascript">
 	$( document ).ready(function() {
+		console.log($("#bbp-forum-566 > li > a").attr("href"));
 		$("#bbp-forum-566 > li > a").attr("href", "#");
 	});
 </script>
