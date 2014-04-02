64445d72bd26a5353806800995546452c6be14fc
CeciliaMadrid <lya.madrid@gmail.com>
2014-04-02 10:50:14 -0600

    Publicación estilo título

--- wp-content/themes/wplms/single-nosidebar.php
+++ wp-content/themes/wplms/single-nosidebar.php
@@ -48,7 +48,8 @@ if(isset($title) && $title !='' && $title !='H'){
                     </div>
                     </div>
                     <div class="col-md-9 col-sm-8">
-                    <div class="excerpt2 "><h3><?php the_title(); ?></h3></div>
+                    <div class="blogpost"><h3><?php the_title(); ?></h3></div>
+                    <div class="separador"></div>
                     <?php
                     }
                         the_content();
