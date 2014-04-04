2f038054c1a0e882deb1edc113a019229c6e4e17
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-04-03 20:00:03 -0600

    home dialog

--- wp-content/themes/wplms/page-24.php
+++ wp-content/themes/wplms/page-24.php
@@ -151,7 +151,12 @@ if(isset($title) && $title !='' && $title !='H'){
 <?php
 ?>
 </div>
-
+<div id="dialog-call-to-action" title="Sign up!"><p>The GPSA Knowledge Platform offers a great diversity of learning, sharing and networking activities.<br/>All these activities are free of charge and open to everyone.<br/>However, to participate you need be registered. <a href="http://gpsaknowledge.org/register/" title="Create an account"><strong>Click here to create your own account</strong></a></p></div>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+<script>jQuery(function() { 
+	jQuery( "#dialog-call-to-action" ).dialog({autoOpen: false,show: {effect: "blind",duration: 1000},hide: {effect: "explode",duration: 1000}}); 
+	jQuery("#dialog-call-to-action").dialog("open");});
+</script>
 <?php
 get_footer();
 ?>
