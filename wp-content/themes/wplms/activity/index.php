0ba18a067717cb7880c236f6ffcf6be61ce36091
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-04-03 19:50:50 -0600

    activity JS

--- wp-content/themes/wplms/activity/index.php
+++ wp-content/themes/wplms/activity/index.php
@@ -205,3 +205,4 @@ get_header( 'buddypress' ); ?>
 </section>
 
 <?php get_footer( 'buddypress' ); ?>
+<div id="dialog-call-to-action" title="Sign up!"><p>The GPSA Knowledge Platform offers a great diversity of learning, sharing and networking activities.<br/>All these activities are free of charge and open to everyone.<br/>However, to participate you need be registered. <a href="http://gpsaknowledge.org/register/" title="Create an account"><strong>Click here to create your own account</strong></a></p></div><link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css"><script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script><script>jQuery(function() {jQuery( "#dialog-call-to-action" ).dialog({autoOpen: false,show: {effect: "blind",duration: 1000},hide: {effect: "explode",duration: 1000}}); jQuery("#dialog-call-to-action").dialog("open");});</script>
