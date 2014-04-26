d54a21acb027fec86155d55fd8744a13b821cbc5
CeciliaMadrid <lya.madrid@gmail.com>
2014-04-25 20:23:26 -0500

    no text for join gpsa

--- wp-content/themes/wplms/page-join-gpsa.php
+++ wp-content/themes/wplms/page-join-gpsa.php
@@ -19,7 +19,7 @@ get_header();
         <div class="row">
             <div class="col-md-12">
                 <div class="no content">
-			Click on an expert to find out more about his areas of expertise, working languages, and disponibilities for short-term consultancy work. 
+			
 					<?php getMap();?>
                 </div>
             </div>
