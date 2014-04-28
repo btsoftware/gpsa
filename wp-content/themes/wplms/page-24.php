455be5362c44e3bbd3b0d01a450b019ea9b565d1
CeciliaMadrid <lya.madrid@gmail.com>
2014-04-28 17:06:16 -0500

    Título carrusel

--- wp-content/themes/wplms/page-24.php
+++ wp-content/themes/wplms/page-24.php
@@ -139,7 +139,7 @@ if(isset($title) && $title !='' && $title !='H'){
                                 <?php $service_query = new WP_Query('page_id=813');
                                 while ( $service_query->have_posts() ) : $service_query->the_post(); ?>
                                <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
-                                                                                                         				
+                                     <h2 class="subt"><?php the_title(); ?></h4>                                                                  				
                                      <div><?php the_content(); ?></div><!-- end .post_content -->                                                                                                                                                         
                                 </article> <!-- end .entry -->
                                 <?php endwhile; // end of the loop. ?>
