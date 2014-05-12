526a979089c6c22305fc2f73403cf6f04bff7c36
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-05-12 12:30:19 -0500

    text roster functions

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -103,6 +103,7 @@ function getMap() {
 	echo "<script src='/map/js/gpsa-rosters.geojson.js' type='text/javascript'></script>";
 	
 	echo "<a name='roster-of-practitioners'></a><div class='pagetitle'><h2>Roster of practitioners</h2></div>";
+	echo "<p>Click on an expert to find out more about his areas of expertise, working languages, and disponibilities for short-term consultancy work.</p>";
 	echo "<div id='map'><div id='themes-layers' class='layers'></div><div id='info'></div></div>";
 	echo "<script src='/map/js/map-init.js' type='text/javascript'></script>";
 }
