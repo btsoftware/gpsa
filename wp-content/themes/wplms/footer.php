4c0630d9a0a5446a96eb157d3a9b4eb68ac8db85
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2014-07-29 18:00:55 -0500

    footer link}

--- wp-content/themes/wplms/footer.php
+++ wp-content/themes/wplms/footer.php
@@ -104,7 +104,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">              
-                <?php $copyright=vibe_get_option('copyright'); echo (isset($copyright)?$copyright:'&copy; 2013, All rights reserved.'); ?>, Developed by Fundar, Centro de Análisis e Investigación
+                <?php $copyright=vibe_get_option('copyright'); echo (isset($copyright)?$copyright:'&copy; 2013, All rights reserved.'); ?>, Developed by 
+                <a href="http://fundar.org.mx"  style="text-decoration:none;" title="Fundar, Centro de Análisis e Investigación">Fundar, Centro de Análisis e Investigación</a>
             </div>
             <div class="col-md-4">
                 <div id="footermenu">
