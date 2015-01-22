3ba69b8c01a8dfacd7a8eeb7bd37bf59840439af
josechajob <josecha>
2015-01-22 11:48:47 -0600

    add function topics order by title
    add function topics order by title

--- wp-content/plugins/bbpress/templates/default/bbpress/loop-topics.php
+++ wp-content/plugins/bbpress/templates/default/bbpress/loop-topics.php
@@ -11,6 +11,16 @@
 
 <?php do_action( 'bbp_template_before_topics_loop' ); ?>
 
+<?php 
+if(bbp_get_forum_topic_count()>0)
+    {       
+
+        $bbp_loop_args = array(
+        'orderby' => 'title',
+        'order' => 'DESC',
+        );
+        ?>
+
 <ul id="bbp-forum-<?php bbp_forum_id(); ?>" class="bbp-topics">
 
 	<li class="bbp-header">
@@ -25,13 +35,13 @@
 	</li>
 
 	<li class="bbp-body">
+    <?php if ( bbp_has_topics( $bbp_loop_args ) ) : ?>
+        <?php while ( bbp_topics() ) : bbp_the_topic(); ?>
 
-		<?php while ( bbp_topics() ) : bbp_the_topic(); ?>
+            <?php bbp_get_template_part( 'loop', 'single-topic' ); ?>
 
-			<?php bbp_get_template_part( 'loop', 'single-topic' ); ?>
-
-		<?php endwhile; ?>
-
+        <?php endwhile; ?>
+    <?php endif;?>
 	</li>
 
 	<li class="bbp-footer">
