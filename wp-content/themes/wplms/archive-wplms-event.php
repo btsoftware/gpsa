165e15ee12d8b0bdc4b67c745892b59ef4f39b50
josechajob <josecha>
2015-03-05 21:08:18 -0600

    Revert "archive event"
    This reverts commit 8509716a9495c6aa40dd4955f6901a524078de0f.

--- wp-content/themes/wplms/archive-wplms-event.php
+++ wp-content/themes/wplms/archive-wplms-event.php
@@ -59,11 +59,3 @@ get_header();
 <?php
 get_footer();
 ?>
-
-<script type="text/javascript">
-$(document).ready(function(){ 
-   $('#alternar-respuesta-ej1').on('click',function(){
-      $('#respuesta-ej1').toggle();
-   });
-});
-</script>
