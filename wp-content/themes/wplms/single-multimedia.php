dc22a990309f9d24169c0608b0b7a8ab9d6b32c2
CecilaMadrid <lya.madrid@gmail.com>
2015-06-21 23:09:44 -0500

    template metrials multimedia

--- wp-content/themes/wplms/single-multimedia.php
+++ wp-content/themes/wplms/single-multimedia.php
@@ -53,16 +53,7 @@ if(isset($title) && $title !='' && $title !='H'){
                     <div class="publicacionpost"><h3><?php the_title(); ?></h3></div>
                     <div class="separador"></div>
                                         
-                     <div class="tags">
-                    <div class="inpublication">
-                        <i class="icon-user clicked left-i p12"></i><p class="autor_material"><?php echo get_post_meta($post->ID, 'publication_author', true); ?></p></div>
-                    <div class="inpublication">
-                        <i class="icon-book-open-1 p13 left-i"></i>
-                        <p class="autor_material">Published by: <?php echo get_post_meta($post->ID, 'publication_by', true); ?> </p>                         
-                    </div>
-                       
-                    <div class="inpublication"><i class="icon-clock left-i"></i><?php echo get_post_meta($post->ID, 'publication_year', true); ?>
-                    </div>           
+                    <div class="tags">                                
                     <div class="inpublication"><i class="icon-script clicked p12 rignt-i"></i>
                         <?php
                          $terms = get_terms('Material Type');
