49d4efcf8ea8c17a1a4160f8dcf09153333bb256
CecilaMadrid <lya.madrid@gmail.com>
2015-06-21 23:27:53 -0500

    templete materials

--- wp-content/themes/wplms/single-multimedia.php
+++ wp-content/themes/wplms/single-multimedia.php
@@ -43,8 +43,7 @@ if(isset($title) && $title !='' && $title !='H'){
         <div class="row">         
                
                 <div class="content top-puplicaciones ">
-                    <div class="col-md-9 col-sm-8">
-                    <?php if (have_posts()) : ?>
+                    <div class="col-md-9 col-sm-8">                 
                     <div id="post-<?php $id=the_ID(); ?>" <?php post_class(); ?>>
                     <div class="publicacionpost"><h3><?php the_title(); ?></h3></div>
                     <div class="separador"></div>
