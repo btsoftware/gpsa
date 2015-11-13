f0b3dd2289e8fbf9ee7d4fedd8ad7b4d467baabd
CecilaMadrid <lya.madrid@gmail.com>
2015-11-12 19:45:57 -0600

    probando

--- wp-content/themes/wplms/single-blog.php
+++ wp-content/themes/wplms/single-blog.php
@@ -61,8 +61,8 @@ if(isset($title) && $title !='' && $title !='H'){
                      ?>
                     
                     <div class="adthis"><?php do_action( 'addthis_widget', get_permalink(), get_the_title(), 'small_toolbox'); ?></div>
+                    dfagrtghhh
 
-                
                     <?php
                         
                     ?>
