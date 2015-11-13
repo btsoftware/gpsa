5a307d33a9cd49b6a60abb0f22fd8408aaf389c1
CecilaMadrid <lya.madrid@gmail.com>
2015-11-12 19:44:42 -0600

    corrigiendo

--- wp-content/themes/wplms/single-blog.php
+++ wp-content/themes/wplms/single-blog.php
@@ -64,7 +64,7 @@ if(isset($title) && $title !='' && $title !='H'){
 
                 
                     <?php
-                        }
+                        
                     ?>
                 </div>
 
