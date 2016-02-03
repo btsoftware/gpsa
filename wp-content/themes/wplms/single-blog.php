e751518cb65b64a89cdc1e127c8a0dbd994f3238
CecilaMadrid <lya.madrid@gmail.com>
2016-02-03 12:41:25 -0600

    commentaries en content single post

--- wp-content/themes/wplms/single-blog.php
+++ wp-content/themes/wplms/single-blog.php
@@ -73,6 +73,38 @@ if(isset($title) && $title !='' && $title !='H'){
                                 <p class="autor-nombre"><?php echo get_post_meta($post->ID, 'nombre_del_author', true); ?></p>
                                 <?php echo get_post_meta($post->ID, 'trayectoria_author', true); ?>
                             </div>
+                            <!-- comentarios -->
+                                            <?php
+                $author = getPostMeta($post->ID,'vibe_author',true);
+                if(vibe_validate($author)){?>
+                <div class="postauthor">
+                    <div class="auth_image">
+                        <?php
+                            echo get_avatar( get_the_author_meta('email'), '80');
+                         ?>
+                    </div>
+                    <div class="author_info">
+                        <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="readmore link"><?php printf(__('Courses from %s','vibe'),get_the_author_meta( 'display_name' )); ?></a>
+                        <h6><?php the_author_meta( 'display_name' ); ?></h6>
+                        <div class="author_desc">
+                             <?php  the_author_meta( 'description' );?>
+
+                             <p class="website">Website : <a href="<?php  the_author_meta( 'url' );?>" target="_blank"><?php  the_author_meta( 'url' );?></a></p>
+                                     <?php
+                            $author_id=  get_the_author_meta('ID');
+                            vibe_author_social_icons($author_id);
+                        ?>  
+                            
+                        </div>     
+                    </div>    
+                </div>
+                <?php
+                }
+                
+                comments_template();
+                endwhile;
+                endif;
+                ?>
                         </div>  
                     </div>
  <?php
