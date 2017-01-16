269830e1dda804c0ce98ae70f72513c8466bface
josechajob <josecha>
2017-01-16 13:50:02 -0600

    stielo remove

--- wp-content/themes/wplms/testmap-page.php
+++ wp-content/themes/wplms/testmap-page.php
@@ -32,12 +32,6 @@ if(isset($title) && $title !='' && $title !='H'){
 <?php
 }
 ?>
-<script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
-
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/colorbox.css">
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.colorbox-min.js"></script>
 
 
 <!-- no cargarlos si la pantalla es menor a 600 -->
