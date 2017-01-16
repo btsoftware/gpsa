b5cd5f3c684643c094697eb26d9d70f1fdf4d0aa
josechajob <josecha>
2017-01-16 13:21:33 -0600

    header

--- wp-content/themes/wplms/testmap-page.php
+++ wp-content/themes/wplms/testmap-page.php
@@ -3,12 +3,69 @@
  * Template Name: testmap
  */
 
-//get_header();
+get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
 $title=get_post_meta(get_the_ID(),'vibe_title',true);
 if(isset($title) && $title !='' && $title !='H'){
 ?>
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
+
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/colorbox.css">
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.colorbox-min.js"></script>
+
+
+<!-- no cargarlos si la pantalla es menor a 600 -->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/d3.min.js"></script>
+
+<!--script src="http://d3js.org/d3.geo.projection.v0.min.js"></script-->
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/d3.geo.projection.v0.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/topojson.v1.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/map_d3.js"></script>
+<!-- no cargarlos si la pantalla es menor a 600 -->
+
+
+
+<script>
+$(document).ready(function(){
+    var countries = ['Mexico'];
+    
+    /*Agregando paises a la lista*/
+    for(var i = 0 in countries){
+        jQuery('#menu-paises ul').append("<li class='pais'> <a href='#" + countries[i] + "'>" + countries[i] + "</a> </li>")
+    }
+    if ($(window).width() > 600) {
+      var width = document.getElementById('map').offsetWidth 
+        , height = document.getElementById('map').offsetHeight 
+        , topo, projection, path, svg, g
+        , tooltip = d3.select("#map").append("div").attr("class", "tooltip hidden");
+      
+      d3.json("<?php echo get_template_directory_uri(); ?>/assets/js/data/world-topo.json.packed", function(error, world) {
+            jQuery(".wait").remove()
+            setup(width, height);
+            topo = topojson.feature(world, world.objects.countries).features;
+            draw(topo, tooltip, countries); 
+      });
+    }
+
+    jQuery(".close-map-info").on("click", function(e){
+        jQuery("#map-info").toggle( "slide", { "direction": "down", "duration": 800  });
+    })
+
+    jQuery("li.pais a").on("click", function(e){
+        var pais = jQuery(this).attr("href").substring(1);
+        search_stories_by_country(pais)
+        return false
+    })
+
+    $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+})
+</script>
+
+
 <section id="title">
     <div class="container">
          <div class="row">
@@ -69,62 +126,6 @@ if(isset($title) && $title !='' && $title !='H'){
 endwhile;
 endif; 
 ?>
-<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-<script src="//code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
-
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css">
-
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/colorbox.css">
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/jquery.colorbox-min.js"></script>
-
-
-<!-- no cargarlos si la pantalla es menor a 600 -->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/d3.min.js"></script>
-
-<!--script src="http://d3js.org/d3.geo.projection.v0.min.js"></script-->
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/d3.geo.projection.v0.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/libs/topojson.v1.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/assets/js/map_d3.js"></script>
-<!-- no cargarlos si la pantalla es menor a 600 -->
-
-
-
-<script>
-$(document).ready(function(){
-    var countries = ['Mexico'];
-    
-    /*Agregando paises a la lista*/
-    for(var i = 0 in countries){
-        jQuery('#menu-paises ul').append("<li class='pais'> <a href='#" + countries[i] + "'>" + countries[i] + "</a> </li>")
-    }
-    if ($(window).width() > 600) {
-      var width = document.getElementById('map').offsetWidth 
-        , height = document.getElementById('map').offsetHeight 
-        , topo, projection, path, svg, g
-        , tooltip = d3.select("#map").append("div").attr("class", "tooltip hidden");
-      
-      d3.json("<?php echo get_template_directory_uri(); ?>/assets/js/data/world-topo.json.packed", function(error, world) {
-            jQuery(".wait").remove()
-            setup(width, height);
-            topo = topojson.feature(world, world.objects.countries).features;
-            draw(topo, tooltip, countries); 
-      });
-    }
-
-    jQuery(".close-map-info").on("click", function(e){
-        jQuery("#map-info").toggle( "slide", { "direction": "down", "duration": 800  });
-    })
-
-    jQuery("li.pais a").on("click", function(e){
-        var pais = jQuery(this).attr("href").substring(1);
-        search_stories_by_country(pais)
-        return false
-    })
-
-    $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
-})
-</script>
-
 
 <?php
 get_footer();
