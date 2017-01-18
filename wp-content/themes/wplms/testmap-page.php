2522a6cb5577d0d8daf9adcb3b69378816a297f9
CecilaMadrid <lya.madrid@gmail.com>
2017-01-18 08:01:00 -0600

    ajuste estilo botones tell your story

--- wp-content/themes/wplms/testmap-page.php
+++ wp-content/themes/wplms/testmap-page.php
@@ -61,9 +61,9 @@ if(isset($title) && $title !='' && $title !='H'){
 	    </div>         
             
             <div class="col-md-12" style="margin-top: -11px; margin-bottom: 10px;">
-                <a class="contorno-morado" href="/share-video">Share your story</a>
+                <a class="contorno-morado-tell" href="/share-video">Share your story</a>
                    
-                <a class="contorno-morado" href="http://www.youtube.com/embed/3_1m5dLuJ9k?rel=0&amp;wmode=transparent">
+                <a class="contorno-morado-tell" href="http://www.youtube.com/embed/3_1m5dLuJ9k?rel=0&amp;wmode=transparent">
                     <span class="tutorial">Video Tutorial </span> <span id ="play">►</span> 
                 </a>
             </div>
