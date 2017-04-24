e11373070ea70b15120e194851a854dadbcd578c
josechajob <josecha>
2017-04-24 11:18:59 -0500

    equis

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -172,7 +172,7 @@ function getSurvey() {
 	echo '<div id="survey" class="border">
 		<div class="title-survey border">
 		&nbsp;&nbsp;<a class="contorno-morado" href="#close">X</a>
-			<span>Tell us what you think!</span>
+			<span>Tell us what you think!                        </span>
 
 			</div>
 		<div class="content-survey">
