51ffefa3f455e3207b9320cd33a5b82c26ecef0d
josechajob <josecha>
2017-05-10 15:28:00 -0500

    stories

--- wp-content/themes/wplms/functions.php
+++ wp-content/themes/wplms/functions.php
@@ -304,7 +304,9 @@ function stories_init() {
     'labels' => array(
 	    'name' => __( 'Stories' ),
 	    'singular_name' => __( 'Story' ),
-	    'add_new' => __( 'Add New Story' ),
+	    'singular_label' => __( 'Story' ),        
+	    'all_items' => __('Stories'),
+        'add_new' => __( 'Add New Story' ),
 	    'add_new_item' => __( 'Add New Story' ),
 	    'edit_item' => __( 'Edit Story' ),
 	    'new_item' => __( 'Add New Story' ),
@@ -317,19 +319,20 @@ function stories_init() {
       'has_archive' => true,
       'show_ui' => true,
       'capability_type' => 'post',
-      'hierarchical' => false,
-      'rewrite' => array('slug' => 'stories'),
+      'hierarchical' => true,
+      'rewrite' => array('slug' => 'stories', 'with_front' => false),
       'query_var' => true,
       'menu_position' => 5,
+ 	 'taxonomies' => array("story_category"),
       'menu_icon' => 'dashicons-portfolio',
       'supports' => array(
           'title',
           'excerpt',
           'editor'
-          //'custom-fields',
-          //'revisions',
-          //'author',
-          //'page-attributes',
+          'custom-fields',
+          'revisions',
+          'author',
+          'page-attributes',
       )
  	);
   register_post_type( 'stories', $args );
