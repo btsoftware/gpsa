b827fb1f969702e273e207183150408374644672
josechajob <josecha>
2017-10-05 11:42:28 -0500

    browser

--- wp-content/themes/wplms/header.php
+++ wp-content/themes/wplms/header.php
@@ -18,7 +18,7 @@ wp_head();
 ?>
 
 </head>
-<div id="bar"><center>Your browser(Internet Explorer) is out-of-date. Please updated it to the latest version or download one of these up-to-date, free and excellent browsers: Chrome or Firefox.</center></div>
+
 <body <?php body_class(); ?>>
 <div id="global" class="global">
     <div class="pagesidebar">
@@ -131,23 +131,4 @@ wp_head();
             </div>
         </header>
 		
-		<script type="text/javascript">
-var trident = !!navigator.userAgent.match(/Trident\/7.0/);
-var net = !!navigator.userAgent.match(/.NET4.0E/);
-var IE11 = trident && net
-var IEold = ( navigator.userAgent.match(/MSIE/i) ? true : false );
-
-
-if(IE11 || IEold){
-
-}
-else{
-document.getElementById("bar").style.display = "none";
-}
-var Retraso = 10000 ;
-function Desa(){ 
-document.getElementById("bar").style.display = "none";
-}; 
-window.load=setTimeout('Desa()', Retraso); 
-
-</script>
+	
