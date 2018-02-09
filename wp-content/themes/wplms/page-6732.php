0a336718d297838f3629458b5ae1692018094a13
josechajob <josech@fundar.org.mx>
2018-02-09 12:45:18 -0600

    array query

--- wp-content/themes/wplms/page-6732.php
+++ wp-content/themes/wplms/page-6732.php
@@ -99,13 +99,13 @@ add_filter( 'bp_activity_excerpt_append_text', 'cc_excerpt_append_text' );
 		              <h2 style="text-align: center;font-size: 36px;color: #253169;">Forums</h2>
 		 </div>
 
-<?php $argus = array( 'post_type' => 'topic', 'posts_per_page' => 6, 'post_status' => 'closed', 'post_parent' => 566, 'orderby' => 'title', 'order'   => 'DESC',);?>
+<?php $argus = array( 'post_type' => 'topic', 'posts_per_page' => 4, 'post_status' => 'closed', 'post_parent' => 566, 'orderby' => 'title', 'order'   => 'DESC',);?>
 
                                 <?php
 								$query = new WP_Query($argus);
 
                                 while ( $query->have_posts() ) : $query->the_post(); 
-								//the_title();
+							print_r($query); //the_title();
 								echo '<div class="entry-content">';
 								//the_content();
 								echo '</div>';
@@ -117,7 +117,7 @@ add_filter( 'bp_activity_excerpt_append_text', 'cc_excerpt_append_text' );
                                                                                                              
                                                  </div> 	<!-- end .post-thumbnail -->					
                                                  <div class="">						
-                                                     <?php the_content(); ?>
+                                                     <?php //the_content(); ?>
                                                   </div> 	<!-- end .post_content -->                                                                                  
                                                 
 			            </article> <!-- end .entry -->
